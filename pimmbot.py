22e3f49ea8453f61607e11118679cce49d568fbf
jisuk <zeya9643@naver.com>
2020-09-29 08:07:05 +0900

    no message

--- pimmbot.py
+++ pimmbot.py
@@ -2,10 +2,7 @@ import asyncio, discord, os
 from discord.ext import commands, tasks
 
 bot = commands.Bot(command_prefix='!')
-member_path = os.path.dirname(os.path.abspath(__file__))+"\member.txt"
-m = open(member_path, 'r', encoding="utf-8")
-member = m.read().split()
-
+member = []
 attendance = []
 
 @bot.event
@@ -14,13 +11,10 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    for i in range (len(member)):
-        attendance.append(False)
-    member.sort()
 
 @bot.command()
 async def 도움말(ctx):
-    await ctx.send('!출석 이름\t-출결 상태를 출석으로 바꿉니다\n!출석확인\t-출석인원 수와 출결현황을 확인합니다\n!출석초기화\t-출결현황을 초기화합니다(회의가 시작하거나 끝났을 때 입력)')
+    await ctx.send('!출석 이름\t-출결 상태를 출석으로 바꿉니다\n!출석확인\t-출석인원 수와 출결현황을 확인합니다\n!출석초기화\t-출결현황을 초기화합니다(회의가 시작하거나 끝났을 때 입력)\n!등록 이름\t-리스트에 회원을 등록합니다\n!제거 이름\t-회원을 리스트에서 제거합니다')
 
 @bot.command()
 async def 출석(ctx, arg):
@@ -32,13 +26,12 @@ async def 출석(ctx, arg):
     
 @bot.command()
 async def 출석확인(ctx):
-    
-    for i in range(0, len(member)):
-        if attendance[i]:
-            await ctx.send(f'{member[i]} : 출석')
-        else:
-            await ctx.send(f'{member[i]} : 부재')
+    await ctx.send(f"총원 {len(attendance)}명")
     await ctx.send(f"출석인원 {attendance.count(True)}명")
+    await ctx.send(f"부재인원 {attendance.count(False)}명")
+    for i in range(0, len(member)):
+        if attendance[i] == False:
+            await ctx.send(f'{member[i]}')
 
 @bot.command()
 async def 출석초기화(ctx):
@@ -47,15 +40,23 @@ async def 출석초기화(ctx):
     await ctx.send('출석목록 초기화')
 
 @bot.command()
-async def 신규등록(ctx, arg):
+async def 등록(ctx, arg):
     if arg in member:
         await ctx.send(f'{arg} 님은 이미 등록되어 있습니다')
     else:
         member.append(arg)
         member.sort()
-        newMemberIndex = member.index(arg)
-        attendance.insert(newMemberIndex, False)
-
+        attendance.insert(member.index(arg), False)
         await ctx.send(f'{arg} 님이 등록되었습니다')
 
-bot.run(os.environ['token'])
+
+@bot.command()
+async def 제거(ctx, arg):
+    if arg in member:
+        index = member.index(arg)
+        del attendance[index]
+        del member[index]
+        await ctx.send(f'{arg} 님을 제거합니다')
+    else:
+        await ctx.send(f'{arg} 님은 등록되어 있지 않습니다')
+bot.run(token)
